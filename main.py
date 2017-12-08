e4a29043a0c35c43f6439823509ef3be64b3ec43
vovanthi <vovanthi@earth.sint.co.jp>
2017-12-08 17:27:13 +0900

    trail

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from flask import Flask, jsonify, abort, make_response
 import json
+import sys
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello, World!' + sys.version
 
-@app.route('/inference', methods=['GET'])
-def inference():
-  #推論処理
-  #仮結果
-  result = {
-        "result":True,
-        "data":{
-            "hana1":"桜",
-            "hana2":"梅",
-            "hana3":"ひまわり"
-            }
-        }
-  return make_response(json.dumps(result, ensure_ascii=False))
+#@app.route('/inference', methods=['GET'])
+#def inference():
+#  result = {
+#        "result":True,
+#        "data":{
+#            "hana1":"桜",
+#            "hana2":"梅",
+#            "hana3":"ひまわり"
+#            }
+#        }
+#  return make_response(json.dumps(result, ensure_ascii=False))
 
 if __name__ == '__main__':
   app.run()
