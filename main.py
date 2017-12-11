2348e93d031ae7f038ff0618534f4bfff563871e
vovanthi <vovanthi@earth.sint.co.jp>
2017-12-11 17:06:39 +0900

    fix main

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello, World!' + sys.version
 
-#@app.route('/inference', methods=['GET'])
-#def inference():
-#  result = {
-#        "result":True,
-#        "data":{
-#            "hana1":"桜",
-#            "hana2":"梅",
-#            "hana3":"ひまわり"
-#            }
-#        }
-#  return make_response(json.dumps(result, ensure_ascii=False))
+@app.route('/inference', methods=['GET'])
+def inference():
+  result = {
+        "result":True,
+        "data":{
+            "hana1":"桜",
+            "hana2":"梅",
+            "hana3":"ひまわり"
+            }
+        }
+  return make_response(json.dumps(result, ensure_ascii=False))
 
-@app.route('/inference', methods=['POST'])
-def inference_binary():
-  return 'coming soon'
+#@app.route('/inference', methods=['POST'])
+#def inference_binary():
+#  return 'coming soon'
 
 if __name__ == '__main__':
   app.run()
