be02d87e0b2e1479041aebb98be17972e3e20db5
Hiroko Matsumoto <h.matsumoto@sint.co.jp>
2017-12-13 11:55:29 +0900

    multipul file

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, jsonify, abort, make_response
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
+
 import json
 import sys
 import cntk
 import logs # Debug
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True # Debug
@@ -38,26 +40,34 @@ def test():
 
 @app.route('/blob', methods=['GET'])
 def blob():
-    block_blob_service = BlockBlobService(account_name='hanastragetest', account_key='acount_key')
+    static_dir_path = "D:\home\site\wwwroot\static"
+    account_name = 'hanastragetest'
+    account_key = 'acount_key'
+    container_name = 'images'
+
+    block_blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
     app.logger.info("test message : {}".format(block_blob_service))
     # container create
-    block_blob_service.create_container('images')
-    block_blob_service.set_container_acl('images', public_access=PublicAccess.Container)
+    block_blob_service.create_container(container_name)
+    block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
     #app.logger.info("finish : block_blob_service.set_container_acl")
 
-    # delete
-    block_blob_service.delete_blob('images', 'sunflower.png')
-    
-    # blob write
-    block_blob_service.create_blob_from_path(
-        'images',
-        'sunflower.png',
-        'D:\home\site\wwwroot\static\sunflower.png',
-        content_settings=ContentSettings(content_type='image/png')
-                )
+    files = os.listdir(static_dir_path)
+    for file in files:
+        # delete
+        if block_blob_service.exists(container_name, file):
+            block_blob_service.delete_blob(container_name, file)
+
+        # blob write
+        block_blob_service.create_blob_from_path(
+            container_name,
+            file,
+            static_dir_path + '\\' + file,
+            content_settings=ContentSettings(content_type='image/png')
+                    )
 
     # get container
-    generator = block_blob_service.list_blobs('images')
+    generator = block_blob_service.list_blobs(container_name)
     for blob in generator:
         app.logger.info("generator : {}".format(blob.name))
     #app.logger.info("generator_object : {}".format(generator))
